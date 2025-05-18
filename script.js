e9e8a842e2bc16aac21aebb3d92c28f83b51a2b1
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 16:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const element=document.getElementById("level");
+const findLevel=(element,level)=>{
+    if(element===null){
+        alert(`The level of the element is: ${level}`)
+    }
+    element=element.parentElement;
+    findLevel(element,level+1)
+}
+findLevel(element,0)
